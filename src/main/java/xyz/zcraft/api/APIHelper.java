061397dc410685b97df6253ea3e8a416ee29a805
ZayrexDev <zay.rex@qq.com>
2026-05-07 12:32:23 +0800

    修改格式

--- src/main/java/xyz/zcraft/api/APIHelper.java
+++ src/main/java/xyz/zcraft/api/APIHelper.java
@@ -352,7 +352,7 @@ public class APIHelper {
             for (int i = (query.page() - 1) * 10; i < Math.min(items.size(), query.page() * 10); i++) {
                 SearchResultItem item = items.get(i);
                 sb.append("> ");
-                sb.append(i + 1).append(". ").append(item.beatmapsetId()).append(" - ").append(item.artist()).append(" - ").append(item.title())
+                sb.append(i + 1).append("# ").append(item.beatmapsetId()).append(" - ").append(item.artist()).append(" - ").append(item.title())
                         .append(" <").append(item.mapperName()).append("> ")
                         .append(String.format("[%.2f★ ~ %.2f★]", item.minStar(), item.maxStar()))
                         .append("\n");
