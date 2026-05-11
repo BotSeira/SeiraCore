82e036d7fe3a6479bfb6b4e09452fd6cb158255c
ZayrexDev <zay.rex@qq.com>
2026-05-11 19:06:49 +0800

    修改/sms回复交互方式

--- src/main/java/xyz/zcraft/command/Router.java
+++ src/main/java/xyz/zcraft/command/Router.java
@@ -373,7 +373,7 @@ public class Router {
                         "sms",
                         () -> {
                             Response searchResponse = APIHelper.searchBeatmapSetResponse(searchQuery);
-                            return replyFactory.searchMessage(searchResponse, searchQuery);
+                            return replyFactory.searchMessage(searchResponse);
                         });
             }
             case "lb", "c" -> {
