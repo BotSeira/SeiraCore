052aa2d12f37c9433dfee00f11dcac78fa130849
ZayrexDev <zay.rex@qq.com>
2026-05-13 15:26:55 +0800

    更新版本号至1.3.0

--- src/main/java/xyz/zcraft/util/ApiRequestStats.java
+++ src/main/java/xyz/zcraft/util/ApiRequestStats.java
@@ -17,6 +17,7 @@ public class ApiRequestStats {
                     ? DEFAULT_COST_MILLIS
                     : (long) entry.samples.stream().mapToLong(Long::longValue).average().orElse(DEFAULT_COST_MILLIS);
             entry.pendingCount++;
+            // TODO THIS DON'T SEEM TO BE VERY ACCURATE, CHANGE IT IN THE FUTURE...
             long expectedMillis = avgMillis * Math.min(entry.pendingCount, WINDOW_SIZE);
             return Math.max(1L, (expectedMillis + 999L) / 1000L);
         }
